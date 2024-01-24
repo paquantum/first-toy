6b94009ea87a5b089f7d75e11ce189677d9ce28f
paquantum <enmw34@naver.com>
2024-01-24 19:51:38 +0900

    remove sudo in sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ git pull origin main
 ./gradlew build
 
 CURRENT_PID=$(pgrep -f toy-0.0.1-SNAPSHOT.jar)
-sudo kill -9 $CURRENT_PID
+kill -9 $CURRENT_PID
 
 cd build/libs
 nohup java -jar toy-0.0.1-SNAPSHOT.jar &
