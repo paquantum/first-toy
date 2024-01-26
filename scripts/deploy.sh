86fed213fbc16451f7ab20112c5b4a09e48d65ac
paquantum <enmw34@naver.com>
2024-01-26 22:20:06 +0900

    fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+source ~/.zshrc
+CURRENT_PID=$(pgrep -f toy-0.0.1-SNAPSHOT.jar)
+kill -9 $CURRENT_PID
+./gradlew build
 nohup java -jar build/libs/toy-0.0.1-SNAPSHOT.jar &
+exit
