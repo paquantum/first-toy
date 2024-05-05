fed3d2f2988c952cb2dd157979610014e9ea6e19
paquantum <enmw34@naver.com>
2024-05-05 20:09:53 +0900

    add test load balance

--- src/main/java/first/toy/controller/TestController.java
+++ src/main/java/first/toy/controller/TestController.java
@@ -4,12 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.UUID;
+
 @Controller
 public class TestController {
+
+    public static final UUID uid = UUID.randomUUID();
 
     @RequestMapping("/test")
     @ResponseBody
     public String test() {
-        return "request test to controller";
+        return uid.toString();
     }
 }
